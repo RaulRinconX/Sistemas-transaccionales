8f8d125084d8f7114b48609373c224e69fa195d3
Raul Rincon <r.rincon@uniandes.edu.co>
2023-05-06 00:24:20 -0500

    FIxes

---
+++ iteracion 3/data/RFC5.sql
@@ -0,0 +1,7 @@
+SELECT id_usuario, tipo_usuario, nombre,
+CASE 
+  WHEN EXISTS (SELECT 1 FROM clientes c WHERE c.numero_documento = u.id_usuario) THEN 'Cliente'
+  WHEN EXISTS (SELECT 1 FROM proveedores p WHERE p.id_proveedor = u.id_usuario) THEN 'Proveedor'
+  ELSE 'No especificado'
+END AS uso_usuario
+FROM usuarios u;
---
+++ iteracion 3/data/RFC6.sql
@@ -0,0 +1,6 @@
+SELECT adicionales.nombre as SERVICIO, r.num_reserva, TRUNC(r.fecha_fin) - TRUNC(r.fecha_inicio) AS numero_noches, adicionales.id_oferta, ofertas.precio
+FROM reservas r
+INNER JOIN ofertas ON r.id_oferta = ofertas.id_oferta
+INNER JOIN clientes u ON r.doc_cliente = u.numero_documento
+INNER JOIN adicionales ON ofertas.id_oferta = adicionales.id_oferta
+WHERE u.numero_documento = '9828347';
